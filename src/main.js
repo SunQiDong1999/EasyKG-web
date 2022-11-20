02be59bab41f6669c84a06d31d2d12fb68da81c9
sunqidong <45733296+SunQiDong1999@users.noreply.github.com>
2022-11-20 19:11:28 +0800

    feat: 模型管理（上传文件类型）

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 const app = createApp(App)
 
 import pinia from './store'
+
 app.use(pinia)
 
 import router from './router'
+
 app.use(router)
 
+import JsonViewer from 'vue-json-viewer'
+// Import JsonViewer as a Vue.js plugin
+app.use(JsonViewer)
 /* importElementPlusPlaceholder */
 
 // 自定义指令
 import directive from '@/utils/directive'
+
 directive(app)
 
 // 加载 svg 图标
@@ -20,6 +27,7 @@ import 'virtual:svg-icons-register'
 // 加载 iconify 图标（element plus）
 import { downloadAndInstall } from '@/iconify-ep'
 import useSettingsStore from './store/modules/settings'
+
 if (useSettingsStore().app.iconifyOfflineUse) {
     downloadAndInstall()
 }
